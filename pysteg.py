eb749651fe6acc911f0eb8567496ce869a33e087
Marcin Krzysztof Porwit <mkporwit@porwit.net>
2012-10-25 04:17:55 -0700

    Got lsb encode/decode working

--- pysteg.py
+++ pysteg.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
 
-#import lsbstego
-#import nibabel
+import sys
+import os
+import lsbstego as lsb
+import nibabel as nib
 import argparse
+
+
+def encode(img, args):
+    data = img.get_data()
+    dim = data.shape
+    data = data.ravel()
+
+    if(args.mode == "lsb"):
+        data = lsb.encode(data, args.msg, delim=";")
+    elif(args.mode == "haar"):
+        print "Haar not implemented yet"
+
+    data.shape = (dim)
+    import pdb
+    pdb.set_trace()
+    return nib.Nifti1Image(data, img.get_affine(), img.get_header())
+
+
+def decode(img, args):
+    data = img.get_data()
+    data = data.ravel()
+
+    if(args.mode == "lsb"):
+        msgDataList = lsb.decode(data, delim=";")
+        return lsb.listToText(msgDataList)
+    elif(args.mode == "haar"):
+        return "Haar not implemented yet"
 
 
 def main():
@@ -10,22 +39,29 @@ def main():
 
     subparsers = parser.add_subparsers(help='sub-command help')
     parser_encode = subparsers.add_parser('encode', help='encode help')
-    parser_encode.add_argument('infile', nargs=1, type=argparse.FileType('r'), help='Input image file')
-    parser_encode.add_argument('mode', nargs=1, type=str, choices=["lsb", "haar"], help='Encoding to use')
+    parser_encode.add_argument('imgfile', help='Image file')
+    parser_encode.add_argument('mode', type=str, choices=["lsb", "haar"], help='Encoding to use')
     parser_encode.add_argument('msg', help='Message to encode')
-    parser_encode.add_argument('outfile', nargs=1, type=argparse.FileType('w'), help='file to write encoded image to')
     parser_encode.set_defaults(op='encode')
 
     parser_decode = subparsers.add_parser('decode', help='decode help')
-    parser_decode.add_argument('infile', nargs=1, type=argparse.FileType('r'), help='Input image file containing the message')
-    parser_decode.add_argument('mode', nargs=1, type=str, choices=["lsb", "haar"], help='Decoding to use')
-    parser_decode.add_argument('outfile', nargs=1, type=argparse.FileType('w'), default=sys.stdout, help='file to write decoded message to')
+    parser_decode.add_argument('imgfile', help='Image file containing the message')
+    parser_decode.add_argument('mode', type=str, choices=["lsb", "haar"], help='Decoding to use')
     parser_decode.set_defaults(op='decode')
 
     args = parser.parse_args()
 
-    print args
+    if not os.path.isfile(args.imgfile):
+        print "Can't find input file [{0}]".format(args.imgfile)
+        return 2
+
+    img = nib.load(args.imgfile)
+    if(args.op == 'encode'):
+        img = encode(img, args)
+        nib.save(img, args.imgfile)
+    elif(args.op == 'decode'):
+        msg = decode(img, args)
+        print msg
 
 if __name__ == "__main__":
-    import sys
     sys.exit(main())
