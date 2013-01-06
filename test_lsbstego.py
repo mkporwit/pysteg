9025c1aadb0061e7483706fe8c0a612b24a725dc
Marcin Krzysztof Porwit <mkporwit@porwit.net>
2013-01-06 00:49:45 -0800

    fix lsbstego unittest delim handling

--- test_lsbstego.py
+++ test_lsbstego.py
@@ -5,20 +5,19 @@ import numpy
 
 class TestLsbStego(unittest.TestCase):
     def setUp(self):
-        self.delim = ";"
         self.msg = "It's a trap!"
 
     def testEncodeAndDecode(self):
         data = numpy.zeros(1000, dtype=numpy.uint32)
-        encData = lsbstego.encode(data, self.msg, self.delim)
-        decMsgList = lsbstego.decode(encData, self.delim)
+        encData = lsbstego.encode(data, self.msg)
+        decMsgList = lsbstego.decode(encData)
         decMsg = lsbstego.listToText(decMsgList)
         self.assertEqual(decMsg, "[{0}]".format(self.msg))
 
     def testEncodeAndDecodeOnRandom(self):
         data = numpy.random.randint(0, high=256, size=1000).astype(numpy.uint32)
-        encData = lsbstego.encode(data, self.msg, self.delim)
-        decMsgList = lsbstego.decode(encData, self.delim)
+        encData = lsbstego.encode(data, self.msg)
+        decMsgList = lsbstego.decode(encData)
         decMsg = lsbstego.listToText(decMsgList)
         self.assertEqual(decMsg, "[{0}]".format(self.msg))
 
